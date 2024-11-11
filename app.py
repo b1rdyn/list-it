aefa9c3849157236855c135834b5c3ae70ab3c2c
b1rdyn <lumi.soukko@gmail.com>
2024-11-11 12:41:57 +0200

    templates added

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/mylists")
+def mylists():
+    return render_template("mylists.html")
+
+@app.route("/popularlists")
+def popularlists():
+    return render_template("popularlists.html")
