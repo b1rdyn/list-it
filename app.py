be73abe1a420b88f97cf9e2ce83b6f75c29f9b63
b1rdyn <lumi.soukko@gmail.com>
2024-11-11 12:31:19 +0200

    register.html added
    added first version of register.html. small edits in app.py and index.html

--- app.py
+++ app.py
@@ -5,5 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    words = ["apina", "banaani", "cembalo"]
-    return render_template("index.html", message="Tervetuloa!", items=words)
+    return render_template("index.html")
